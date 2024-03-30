2aebf2969329adaa99f01242b56fd812afe975e4
hgodinho <henrique@hgod.in>
2024-03-30 19:17:51 -0300

    fix(vite-app): checkbox - use icon variable from theme

--- packages/vite-app/src/components/tree/Checkbox.tsx
+++ packages/vite-app/src/components/tree/Checkbox.tsx
@@ -1,16 +1,19 @@
 import * as CheckboxPrimitive from "@radix-ui/react-checkbox";
 import { Square, MinusSquare, XSquare } from "react-feather";
+import { useTheme } from "@/provider";
 
 export function Checkbox(props: CheckboxPrimitive.CheckboxProps & { variant: "all" | "none" | "some" }) {
+    const { sizes: { icon } } = useTheme();
+
     const CheckType = () => {
         switch (props.variant) {
             case "all":
-                return <XSquare size={16} />;
+                return <XSquare size={icon} />;
             case "none":
-                return <Square size={16} />;
+                return <Square size={icon} />;
             case "some":
             default:
-                return <MinusSquare size={16} />;
+                return <MinusSquare size={icon} />;
         }
     }
     return (
