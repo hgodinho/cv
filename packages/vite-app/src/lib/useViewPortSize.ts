fa2457a8dbd9f3f7efc72eb046101b8158d4b915
hgodinho <henrique@hgod.in>
2024-04-08 18:50:07 -0300

    feat(vite-app): add breakpoints to hook useViewPortSize

--- packages/vite-app/src/lib/useViewPortSize.ts
+++ packages/vite-app/src/lib/useViewPortSize.ts
@@ -1,13 +1,38 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useMemo } from "react";
+import theme from "../../tailwind.config";
 
 function getWindowDimensions() {
     const { innerWidth: width, innerHeight: height } = window;
+    const breakpoints = useMemo(() => {
+        const {
+            theme: {
+                screens: { sm, md, lg, xl },
+            },
+        } = theme as unknown as {
+            theme: {
+                screens: {
+                    sm: string | number;
+                    md: string | number;
+                    lg: string | number;
+                    xl: string | number;
+                };
+            };
+        };
+        return {
+            sm: parseInt(sm as string),
+            md: parseInt(md as string),
+            lg: parseInt(lg as string),
+            xl: parseInt(xl as string),
+        };
+    }, []);
+
     return {
         width,
         height,
-        isMobile: width < 768 && width >= 0,
-        isTablet: width < 1024 && width >= 768,
-        isDesktop: width >= 1024,
+        isMobile: width <= breakpoints.sm && width >= 0,
+        isTablet: width <= breakpoints.lg && width >= breakpoints.md,
+        isDesktop: width >= breakpoints.lg,
+        breakpoints,
     };
 }
 
