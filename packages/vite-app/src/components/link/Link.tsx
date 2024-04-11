2c2f8979ff423123fbf2d1dd5f93c71ece08d044
hgodinho <henrique@hgod.in>
2024-04-11 00:30:21 -0300

    feat(vite-app): link target switch on received props, `to` for local links `href` for externals

--- packages/vite-app/src/components/link/Link.tsx
+++ packages/vite-app/src/components/link/Link.tsx
@@ -1,15 +1,33 @@
+import { useMemo } from "react";
 import { tw } from "@/lib";
-import { Link as LinkRouter } from "react-router-dom";
+import {
+    Link as LinkRouter,
+    LinkProps as LinkRouterProps,
+} from "react-router-dom";
 
-export type LinkProps = {
-    href: string;
+export type LinkProps = Omit<LinkRouterProps, "to"> & {
+    href?: string;
+    to?: LinkRouterProps["to"];
 };
 
-export function Link({ href, children }: React.PropsWithChildren<LinkProps>) {
+export function Link({
+    href,
+    children,
+    to,
+    ...rest
+}: React.PropsWithChildren<LinkProps>) {
+    const props = useMemo<LinkRouterProps>(() => {
+        return {
+            ...rest,
+            target: href ? "_blank" : "_self",
+            to: href || to || "",
+        };
+    }, [href, to, rest]);
+
     return (
         <LinkRouter
-            to={href}
             className={tw("text-blue-300", "hover:text-blue-500")}
+            {...props}
         >
             {children}
         </LinkRouter>
