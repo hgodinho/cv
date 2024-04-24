46024f92402c7da4477ad42545a2f5ad5db044c1
hgodinho <henrique@hgod.in>
2024-04-24 19:39:17 -0300

    feat(apps-script): somo more utils functions, including callbacks for more specific tests

--- packages/apps-script/src/utils.js
+++ packages/apps-script/src/utils.js
@@ -5,12 +5,52 @@ function getToken() {
     console.log("access_token:", ScriptApp.getOAuthToken());
 }
 
+function stripTrailingSlash(str) {
+    return str.endsWith("/") ? str.slice(0, -1) : str;
+}
+
 function parseDate(date) {
     return Utilities.formatDate(new Date(date), "GMT", "yyyy-MM-dd");
 }
 
 function logTest(title, message) {
     console.log(`${title}: ${message}`);
+}
+
+function errorTest(title, message) {
+    console.error(`${title}: ${message}`);
+}
+
+function getEntityMap() {
+    return {
+        "&": "&amp;",
+        "<": "&lt;",
+        ">": "&gt;",
+        '"': "&quot;",
+        "'": "&#39;",
+        "/": "&#x2F;",
+        "`": "&#x60;",
+        "=": "&#x3D;",
+    };
+}
+
+/**
+ * Escape HTML entities.
+ * @param {string} str | String to be escaped.
+ * @returns | Escaped string.
+ */
+function escape(str) {
+    const entityMap = getEntityMap();
+    return String(str).replace(/[&<>"'`=\/]/g, (s) => entityMap[s]);
+}
+
+/**
+ * Sanitize string.
+ * @param {string} str | String to be sanitized.
+ * @returns | Sanitized string.
+ */
+function sanitize(str) {
+    return String(str).replace(/[&<>"'`=\/]/g, (s) => "");
 }
 
 function test(title, callback) {
@@ -25,7 +65,7 @@ function expect(toBeExpected, actual) {
             if (actual === expected) {
                 logTest(toBeExpected, "Pass");
             } else {
-                logTest(
+                errorTest(
                     toBeExpected,
                     `Fail: Expected ${JSON.stringify(
                         expected
@@ -33,11 +73,61 @@ function expect(toBeExpected, actual) {
                 );
             }
         },
+        toBeString() {
+            if (typeof actual === "string") {
+                logTest(toBeExpected, "Pass");
+            } else {
+                errorTest(
+                    toBeExpected,
+                    `Fail: Expected string, but received ${typeof actual}`
+                );
+            }
+        },
+        toBeNumber() {
+            if (typeof actual === "number") {
+                logTest(toBeExpected, "Pass");
+            } else {
+                errorTest(
+                    toBeExpected,
+                    `Fail: Expected number, but received ${typeof actual}`
+                );
+            }
+        },
+        toBeBoolean() {
+            if (typeof actual === "boolean") {
+                logTest(toBeExpected, "Pass");
+            } else {
+                errorTest(
+                    toBeExpected,
+                    `Fail: Expected boolean, but received ${typeof actual}`
+                );
+            }
+        },
+        toBeObject() {
+            if (typeof actual === "object") {
+                logTest(toBeExpected, "Pass");
+            } else {
+                errorTest(
+                    toBeExpected,
+                    `Fail: Expected object, but received ${typeof actual}`
+                );
+            }
+        },
+        toBeArray() {
+            if (Array.isArray(actual)) {
+                logTest(toBeExpected, "Pass");
+            } else {
+                errorTest(
+                    toBeExpected,
+                    `Fail: Expected array, but received ${typeof actual}`
+                );
+            }
+        },
         toEqual(expected) {
             if (JSON.stringify(actual) === JSON.stringify(expected)) {
                 logTest(toBeExpected, "Pass");
             } else {
-                logTest(
+                errorTest(
                     toBeExpected,
                     `Fail: Expected ${JSON.stringify(
                         expected
@@ -55,11 +145,29 @@ function expect(toBeExpected, actual) {
             if (error) {
                 logTest(toBeExpected, "Pass");
             } else {
-                logTest(
+                errorTest(
                     toBeExpected,
                     "Fail: Expected function to throw an error"
                 );
             }
         },
+    };
+}
+
+function mockRequest(path = "", parameter = undefined) {
+    let queryString = "";
+    if (parameter) {
+        queryString = Object.entries(parameter).reduce((acc, [key, value]) => {
+            if (acc.length > 0) {
+                acc += "&";
+            }
+            acc += `${key}=${value}`;
+            return acc;
+        }, "");
+    }
+    return {
+        queryString,
+        parameter,
+        pathInfo: path,
     };
 }
