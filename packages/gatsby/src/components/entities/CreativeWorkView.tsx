2abea419c0dde7dff45e4de7b0a9c6d5d594127c
hgodinho <henrique@hgod.in>
2024-10-31 20:41:22 -0300

    feat(gatsby): add new properties to CreativeWorkView

--- packages/gatsby/src/components/entities/CreativeWorkView.tsx
+++ packages/gatsby/src/components/entities/CreativeWorkView.tsx
@@ -35,6 +35,34 @@ export function CreativeWorkView({
                 {classes?.[data.type]}
             </p>
             <Link href={data.id as string}>{data.name}</Link>
+            {typeof data.description !== "undefined" && (
+                <p>{data.description as React.ReactNode}</p>
+            )}
+            {typeof data.publication !== "undefined" && (
+                <p>
+                    {(data.publication as string[])?.map((pub, i) => (
+                        <Fragment key={`pub-${i}`}>{pub}</Fragment>
+                    ))}
+                </p>
+            )}
+            {typeof data.sameAs !== "undefined" && (
+                <ul className={tw("list-[square]", "list-inside")}>
+                    {(data.sameAs as string[])?.map((sameAs, i) => (
+                        <li key={`sameAs-${i}`} className={tw()}>
+                            <Link
+                                href={sameAs}
+                                className={tw(
+                                    "truncate",
+                                    "text-ellipsis",
+                                    "break-all"
+                                )}
+                            >
+                                {sameAs}
+                            </Link>
+                        </li>
+                    ))}
+                </ul>
+            )}
         </div>
     );
 }
